96caccf6f098766da863fc28b1c0118033d6a25c
Darmin Spahic <Spahic@cl.uni-heidelberg.de>
2018-06-25 15:47:42 +0200

    Moved functions between modules

--- src/modules/affixoidExtractor.py
+++ src/modules/affixoidExtractor.py
@@ -16,114 +16,86 @@ License: MIT License
 Version: 1.0
 
 """
-import duden
-from pygermanet import load_germanet
 
 DATA_FILES_PATH = '../../data/'
-DATA_FILES_OUTPUT_PATH = '../../data/statistics/'
+DATA_FILES_PATH_FINAL = '../../data/final/'
 
 
-def affixoid_extractor(affixoid_inventory):
-    """ This function
+def compare_files(affixoid_inventory_1, affixoid_inventory_2, compare=True):
 
-        Args:
+    if compare:
+        file_1 = []
+        file_2 = []
+        with open(affixoid_inventory_1, 'r', encoding='utf-8') as f:
+            for line in f:
+                word = line.strip().split()
+                f_0 = word[0]  # keep full word for easier controlling
+                file_1.append([f_0])
 
+        with open(affixoid_inventory_2, 'r', encoding='utf-8') as f:
+            for line in f:
+                word = line.strip().split()
+                f_0 = word[0]  # keep full word for easier controlling
+                file_2.append([f_0])
 
-        Returns:
+        counter = 0
+        for i in file_1:
+            # print(i)
+            if file_1[counter] == file_2[counter]:
+                pass
+            else:
+                print(file_1[counter], file_2[counter])
+            counter += 1
 
+        print(len(file_1))
+        print(len(file_2))
 
-        Example:
-            >>> affixoid_extractor(DATA_FILES_PATH + 'unique_binary_prefixoid_formations.srt')
-
-    """
-
-    # Empty lists for collecting data
-    affixoids_inventory = []
-
-    with open(affixoid_inventory, 'r', encoding='utf-8') as f:
-        for line in f:
-            word = line.strip()
-            affixoids_inventory.append(word)
-
-    return affixoids_inventory
-
-
-def search_duden(words_inventory):
-    found_array = []
-    not_found_array = []
-
-    for w in words_inventory:
-        word = duden.get(w)
-        if word:
-            try:
-                print(word.name)
-            except:
-                print(word)
-            found_array.append(w)
-        else:
-            not_found_array.append(w)
-
-    print('Found: ', found_array)
-    print('Total found: ', len(found_array))
-    print('--------------------')
-    print('Not found: ', not_found_array)
-    print('Total not found: ', len(not_found_array))
-
-
-def search_germanet(words_inventory):
-    found_array = []
-    not_found_array = []
-
-    gn = load_germanet()
-
-    for t in words_inventory:
-        if len(gn.synsets(t)) > 0:
-            print(gn.synsets(t))
-            found_array.append(t)
-        else:
-            not_found_array.append(t)
-
-    print('Found: ', found_array)
-    print('Total found: ', len(found_array))
-    print('--------------------')
-    print('Not found: ', not_found_array)
-    print('Total not found: ', len(not_found_array))
-
-
-# needed for duden module
-def replace_umlauts(word_list):
-    umlaute = {'ä': 'ae', 'ö': 'oe', 'ü': 'ue', 'Ä': 'Ae', 'Ö': 'Oe', 'Ü': 'Ue', 'ß': 'ss'}
-    new_list = []
-    for word in word_list:
-        no_umlaut = word.translate({ord(k): v for k, v in umlaute.items()})
-        new_list.append(no_umlaut)
-
-    if len(word_list) == len(new_list):
-        return new_list
     else:
-        print('List error')
+        file_1 = []
+        file_2 = []
+        file_3 = []
+        # aList.insert( 3, 2009)
+        with open(affixoid_inventory_1, 'r', encoding='utf-8') as f:
+            for line in f:
+                word = line.strip().split()
+                file_1.append(word)
 
+        with open(affixoid_inventory_2, 'r', encoding='utf-8') as f:
+            for line in f:
+                word = line.strip().split()
+                file_2.append(word)
+
+        counter = 0
+        for i in file_1:
+            if file_1[counter][0] == file_2[counter][0]:
+                # print(file_1[counter][0], file_2[counter][0])
+                print(file_1[counter], file_2[counter])
+                new_line = list(file_2[counter] + file_1[counter][1:])
+                file_3.append(new_line)
+
+            else:
+                exit()
+            counter += 1
+
+        f = open('binary_unique_instance_suffixoid_segmentations.txt', 'w', encoding='utf-8')
+
+        for item in file_3:
+            # ['Abfalldreck', 'Abfall|Dreck', 'Dreck', 'Schmutz', 'N']
+            output_line = item[0]+'\t'+item[1]+'\t'+item[2]+'\t'+item[3]+'\t'+item[4]
+            f.write(output_line + "\n")
+
+        f.close()
+
+        print(file_3)
+        print(len(file_3))
+        # print(file_2)
 
 
 if __name__ == "__main__":
-    # test_array = ['Löffel', 'Ballspiel', 'Anschlagbolzen', 'Anschlußbolzen', 'Anstandspapst', 'ginge']
-    # new_test_array = replace_umlauts(test_array)
-    # print(new_test_array)
-    #
-    # search_duden(test_array)
-    # search_duden(new_test_array)
-    # print('--------------------')
-
-    # print('Extracting prefixoids:')
-    # prefixoids = affixoid_extractor(DATA_FILES_PATH + 'unique_binary_prefixoid_formations.srt')
-    # prefixoids_no_umlauts = replace_umlauts(prefixoids)
-    # search_duden(prefixoids_no_umlauts)
-    # search_germanet(prefixoids)
-    # print('--------------------')
-
-    print('Extracting suffixoids:')
-    suffixoids = affixoid_extractor(DATA_FILES_PATH + 'unique_binary_suffixoid_formations.srt')
-    # suffixoids_no_umlauts = replace_umlauts(suffixoids)
-    # search_duden(suffixoids_no_umlauts)
-    search_germanet(suffixoids)
+    compare_files(DATA_FILES_PATH + 'binary_unique_instance_label_pairs_prefixoids.csv.affixoidal_status.tsv', DATA_FILES_PATH_FINAL + 'binary_unique_instance_prefixoid_segmentations.txt', True)
+    compare_files(DATA_FILES_PATH + 'binary_unique_instance_label_pairs_prefixoids.csv.affixoidal_status.tsv', DATA_FILES_PATH + 'prefixoid_segmentations_smor.txt.manual_splits.tsv', True)
+    compare_files(DATA_FILES_PATH + 'binary_unique_instance_label_pairs_suffixoids.csv.affixoidal_status.tsv', DATA_FILES_PATH_FINAL + 'binary_unique_instance_suffixoid_segmentations.txt', True)
+    compare_files(DATA_FILES_PATH + 'binary_unique_instance_label_pairs_suffixoids.csv.affixoidal_status.tsv', DATA_FILES_PATH + 'suffixoid_segmentations_smor.txt.manual_splits.tsv', True)
+    # compare_files(DATA_FILES_PATH + 'binary_unique_instance_label_pairs_prefixoids.csv.affixoidal_status.tsv', DATA_FILES_PATH + 'prefixoid_segmentations_smor.txt.manual_splits.tsv', False)
+    # compare_files(DATA_FILES_PATH + 'binary_unique_instance_label_pairs_suffixoids.csv.affixoidal_status.tsv', DATA_FILES_PATH + 'suffixoid_segmentations_smor.txt.manual_splits.tsv', False)
 
--- src/modules/affixoidStatistics.py
+++ src/modules/affixoidStatistics.py
@@ -22,6 +22,8 @@ import sys
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+import duden
+from pygermanet import load_germanet
 
 DATA_FILES_PATH = '../../data/'
 DATA_FILES_OUTPUT_PATH = '../../data/statistics/'
@@ -92,6 +94,92 @@ def affixoid_statistics(affixoid_inventory, data_file_path):
     print(sum(affixoids_counter_list))
 
     return [affixoids_inventory, affixoids_counter_list, affixoids_y_counter_list, affixoids_n_counter_list]
+
+
+def affixoid_extractor(affixoid_inventory):
+    """ This function
+
+        Args:
+
+
+        Returns:
+
+
+        Example:
+            >>> affixoid_extractor(DATA_FILES_PATH + 'unique_binary_prefixoid_formations.srt')
+
+    """
+
+    # Empty lists for collecting data
+    affixoids_inventory = []
+
+    with open(affixoid_inventory, 'r', encoding='utf-8') as f:
+        for line in f:
+            word = line.strip()
+            affixoids_inventory.append(word)
+
+    return affixoids_inventory
+
+
+def search_duden(words_inventory):
+    found_array = []
+    not_found_array = []
+
+    for w in words_inventory:
+        word = duden.get(w)
+        if word:
+            try:
+                print(word.name)
+            except:
+                print(word)
+            found_array.append(w)
+        else:
+            not_found_array.append(w)
+
+    print('Found: ', found_array)
+    print('Total found: ', len(found_array))
+    print('--------------------')
+    print('Not found: ', not_found_array)
+    print('Total not found: ', len(not_found_array))
+
+
+def search_germanet(words_inventory):
+    found_array = []
+    not_found_array = []
+
+    gn = load_germanet()
+
+    for t in words_inventory:
+        if len(gn.synsets(t)) > 0:
+            print(gn.synsets(t))
+            found_array.append(t)
+        else:
+            not_found_array.append(t)
+
+    print('Found: ', found_array)
+    print('Total found: ', len(found_array))
+    print('--------------------')
+    print('Not found: ', not_found_array)
+    print('Total not found: ', len(not_found_array))
+
+
+# needed for duden module
+def replace_umlauts(word_list):
+    umlaute = {'ä': 'ae', 'ö': 'oe', 'ü': 'ue', 'Ä': 'Ae', 'Ö': 'Oe', 'Ü': 'Ue', 'ß': 'ss'}
+    if type(word_list) == list:
+        new_list = []
+        for word in word_list:
+            no_umlaut = word.translate({ord(k): v for k, v in umlaute.items()})
+            new_list.append(no_umlaut)
+
+        if len(word_list) == len(new_list):
+            return new_list
+        else:
+            print('List error')
+    if type(word_list) == str:
+        return word_list.translate({ord(k): v for k, v in umlaute.items()})
+    else:
+        print('Replace Umlauts works only on strings and lists')
 
 
 def plot_statistics(arguments, title):
@@ -177,13 +265,33 @@ def enable_print():
 if __name__ == "__main__":
     # disable_print()
     print('Extracting prefixoids:')
-    arguments_1 = affixoid_statistics(DATA_FILES_PATH + 'prefixoid_inventory.txt', DATA_FILES_PATH +
-                                      'binary_unique_instance_label_pairs_prefixoids.csv.affixoidal_status.tsv')
+    arguments_1 = affixoid_statistics(DATA_FILES_PATH + 'prefixoid_inventory.txt', DATA_FILES_PATH + 'binary_unique_instance_label_pairs_prefixoids.csv.affixoidal_status.tsv')
     plot_statistics(arguments_1, 'prefixoid')
 
     print('--------------------')
 
     print('Extracting suffixoids:')
-    arguments_2 = affixoid_statistics(DATA_FILES_PATH + 'suffixoid_inventory.txt', DATA_FILES_PATH +
-                                      'binary_unique_instance_label_pairs_suffixoids.csv.affixoidal_status.tsv')
+    arguments_2 = affixoid_statistics(DATA_FILES_PATH + 'suffixoid_inventory.txt', DATA_FILES_PATH + 'binary_unique_instance_label_pairs_suffixoids.csv.affixoidal_status.tsv')
     plot_statistics(arguments_2, 'suffixoid')
+
+    # test_array = ['Löffel', 'Ballspiel', 'Anschlagbolzen', 'Anschlußbolzen', 'Anstandspapst', 'ginge']
+    # new_test_array = replace_umlauts(test_array)
+    # print(new_test_array)
+    #
+    # search_duden_frequency(test_array)
+    # search_duden_frequency(new_test_array)
+    # print('--------------------')
+
+    print('Extracting prefixoids:')
+    prefixoids = affixoid_extractor(DATA_FILES_PATH + 'unique_binary_prefixoid_formations.srt')
+    prefixoids_no_umlauts = replace_umlauts(prefixoids)
+    # search_duden_frequency(prefixoids_no_umlauts)
+    search_germanet(prefixoids)
+    print('--------------------')
+
+    print('Extracting suffixoids:')
+    suffixoids = affixoid_extractor(DATA_FILES_PATH + 'unique_binary_suffixoid_formations.srt')
+    suffixoids_no_umlauts = replace_umlauts(suffixoids)
+    # search_duden_frequency(suffixoids_no_umlauts)
+    search_germanet(suffixoids)
+
