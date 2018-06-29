3747fe324fe0e7b66f2b9cc75005954a6747f5f8
Darmin Spahic <Spahic@cl.uni-heidelberg.de>
2018-06-29 14:17:21 +0200

    Added cosine similarity calculator

--- src/main.py
+++ src/main.py
@@ -24,6 +24,7 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 # from bs4 import BeautifulSoup
+from sklearn.metrics.pairwise import cosine_similarity
 
 ################
 # PATH SETTINGS
@@ -31,16 +32,22 @@ import matplotlib.pyplot as plt
 DATA_PATH = '../data/'
 DATA_FINAL_PATH = '../data/final/'
 DATA_FEATURES_PATH = '../data/features/'
+DATA_RESSOURCES_PATH = '../res/'
 
 ################
 # FILE SETTINGS
 ################
 FINAL_PREFIXOID_FILE = 'binary_unique_instance_prefixoid_segmentations.txt'
 FINAL_SUFFIXOID_FILE = 'binary_unique_instance_suffixoid_segmentations.txt'
+FREQUENCY_PREFIXOID_FORMATIONS = 'lemma_frequencies_prefixoid_formations.csv'
+FREQUENCY_SUFFIXOID_FORMATIONS = 'lemma_frequencies_suffixoid_formations.csv'
 FREQUENCY_PREFIXOID_LEMMAS = 'prefixoid_lemmas_freqs.csv'
 FREQUENCY_SUFFIXOID_LEMMAS = 'suffixoid_lemmas_freqs.csv'
 FREQUENCY_PREFIXOID_HEADS = 'lemma_frequencies_unique_heads_of_prefixoid_formations.csv'
 FREQUENCY_SUFFIXOID_MODIFIERS = 'modifiers_of_suffixoids_lemmas_freqs.csv'
+
+FAST_TEXT_PREFIXOID_VECTORS = 'fastText/prefixoid-fastText-vectors.txt'
+FAST_TEXT_SUFFIXOID_VECTORS = 'fastText/suffixoid-fastText-vectors.txt'
 
 
 class AffixoidClassifier:
@@ -69,7 +76,7 @@ class AffixoidClassifier:
                 Dictionary with class instances
 
             Example:
-                >>> create_affixoid_inventory(DATA_FINAL_PATH+FINAL_PREFIXOID_FILE, 'Y')
+                >>> self.create_affixoid_inventory(DATA_FINAL_PATH+FINAL_PREFIXOID_FILE, 'Y')
 
         """
         dictionary = {}
@@ -101,7 +108,7 @@ class AffixoidClassifier:
                 List of lines from file
 
             Example:
-                >>> read_file_to_list(DATA_FINAL_PATH+FINAL_PREFIXOID_FILE)
+                >>> self.read_file_to_list(DATA_FINAL_PATH+FINAL_PREFIXOID_FILE)
 
         """
         file_as_list = []
@@ -111,6 +118,32 @@ class AffixoidClassifier:
                 file_as_list.append(word)
 
         return file_as_list
+
+    def write_list_to_file(self, affixoid_list, output_file):
+        """ This function reads a file with affixoids and returns a list of lines from file
+
+            Args:
+                affixoid_list (list): List with affixoid instances
+                output_file (file): Output file
+
+            Returns:
+                Output file
+
+            Example:
+                >>> self.write_list_to_file(['Bilderbuch'], 'out.txt')
+
+        """
+        f = open(output_file, 'w', encoding='utf-8')
+
+        for item in affixoid_list:
+            # ['Abfalldreck', 'Abfall|Dreck', 'Dreck', 'Schmutz', 'N']
+            split_word = self.split_word_at_pipe(item[1])
+            output_line = item[0] + '\t' + split_word[0] + '\t' + split_word[1]
+            f.write(output_line + "\n")
+
+        print('File written to: ', output_file)
+
+        f.close()
 
     def transform_to_binary(self, class_name):
         """ This function transforms class labels to binary indicators
@@ -122,7 +155,7 @@ class AffixoidClassifier:
                 Binary indicator for class label [0,1]
 
             Examples:
-                >>> transform_to_binary('Y')
+                >>> self.transform_to_binary('Y')
 
         """
 
@@ -144,7 +177,7 @@ class AffixoidClassifier:
                 A list of split items
 
             Examples:
-                >>> split_word_at_pipe('Bilderbuch|Absturz')
+                >>> self.split_word_at_pipe('Bilderbuch|Absturz')
 
         """
 
@@ -164,19 +197,19 @@ class AffixoidClassifier:
                 A frequency for a given word from a dictionary
 
             Examples:
-                >>> extract_frequency('Bilderbuch', {'Bilderbuch':30})
+                >>> self.extract_frequency('Bilderbuch', {'Bilderbuch':30})
 
         """
         if word in dictionary.keys():
             value = dictionary[word]
-            return value
+            return int(value)
         else:
             # TODO:
             # Ikone fehlt
             # Bhaishajya fehlt
             # Ostgoten fehlt
             # Splatterfilm fehlt
-            return word
+            return 1
 
     def create_frequency_dictionary(self, frequency_file):
         """ This function creates a dictionary with frequency instances of affixoids
@@ -188,7 +221,7 @@ class AffixoidClassifier:
                 Dictionary with frequency instances of affixoids
 
             Example:
-                >>> create_frequency_dictionary(DATA_PATH+FREQUENCY_PREFIXOID_LEMMAS)
+                >>> self.create_frequency_dictionary(DATA_PATH+FREQUENCY_PREFIXOID_LEMMAS)
 
         """
 
@@ -199,6 +232,31 @@ class AffixoidClassifier:
                 dict_key = word[0]
                 dict_value = word[1]
                 dictionary.update({dict_key: dict_value})
+
+        return dictionary
+
+    def create_vector_dictionary(self, vector_file):
+        """ This function creates a dictionary with frequency instances of affixoids
+
+            Args:
+                frequency_file (file): File with affixoid frequencies
+
+            Returns:
+                Dictionary with frequency instances of affixoids
+
+            Example:
+                >>> self.create_vector_dictionary(DATA_PATH+FREQUENCY_PREFIXOID_LEMMAS)
+
+        """
+
+        dictionary = {}
+        with open(vector_file, 'r', encoding='utf-8') as f:
+            for line in f:
+                word = line.strip().split()
+                dict_key = word[0]
+                dict_value = list(word[1:])
+                dict_value_int = [float(x) for x in dict_value]
+                dictionary.update({dict_key: dict_value_int})
 
         return dictionary
 
@@ -214,7 +272,7 @@ class AffixoidClassifier:
                 Matplotlib images
 
             Example:
-                >>> plot_statistics(y_prefixoids_inventory, n_prefixoids_inventory, 'Prefixoids')
+                >>> self.plot_statistics(y_prefixoids_inventory, n_prefixoids_inventory, 'Prefixoids')
 
         """
 
@@ -309,6 +367,32 @@ class AffixoidClassifier:
     #
     #     return frequency_list
 
+    def calculate_cosine_similarity(self, word_1, word_2, fast_text_vector_dict):
+        """ This function ...
+
+            Args:
+                word_1 (string): 'Bilderbuchhochzeit'
+                word_2 (string): 'Bilderbuch'
+                fast_text_vector_dict (dict):
+
+            Returns:
+                Cosine similarity between word_1 and word_2
+
+            Example:
+                >>> self.calculate_cosine_similarity('Bilderbuchhochzeit', 'Bilderbuch', {'Bilderbuchhochzeit': [1], 'Bilderbuch': [1]}))
+
+        """
+
+        word_1_vec = fast_text_vector_dict[word_1]
+        word_2_vec = fast_text_vector_dict[word_2]
+
+        # print(word_1_vec)
+        # print(word_2_vec)
+        # print(word_1, word_2, cosine_similarity([word_1_vec], [word_2_vec])[0][0])
+        # print("================")
+
+        return cosine_similarity([word_1_vec], [word_2_vec])[0][0]
+
 
 if __name__ == "__main__":
     """
@@ -327,21 +411,37 @@ if __name__ == "__main__":
     feature_1_prefixoids = []  # binary indicator, if affixoid
     feature_2_prefixoids = []  # frequency
     feature_3_prefixoids = []
+    feature_4_prefixoids = []
+    feature_5_prefixoids = []
+    feature_3_prefixoids_formations = PREFIXOIDS.create_frequency_dictionary(DATA_PATH+FREQUENCY_PREFIXOID_FORMATIONS)
     feature_3_prefixoids_lemmas = PREFIXOIDS.create_frequency_dictionary(DATA_PATH+FREQUENCY_PREFIXOID_LEMMAS)
     feature_3_prefixoids_heads = PREFIXOIDS.create_frequency_dictionary(DATA_PATH+FREQUENCY_PREFIXOID_HEADS)
+    feature_5_prefixoid_vector_dict = PREFIXOIDS.create_vector_dictionary(DATA_RESSOURCES_PATH + FAST_TEXT_PREFIXOID_VECTORS)
 
     for i in prefixoid_inventory:
         item_1 = PREFIXOIDS.transform_to_binary(i[-1])
-        item_2 = PREFIXOIDS.extract_frequency(i[-3], feature_3_prefixoids_lemmas)  # PREFIXOIDS.split_word_at_pipe(i[1])[0]
-        item_3 = PREFIXOIDS.extract_frequency(PREFIXOIDS.split_word_at_pipe(i[1])[1], feature_3_prefixoids_heads)
+        item_2 = PREFIXOIDS.extract_frequency(i[0], feature_3_prefixoids_formations)
+        item_3 = PREFIXOIDS.extract_frequency(i[-3], feature_3_prefixoids_lemmas)  # PREFIXOIDS.split_word_at_pipe(i[1])[0]
+        item_4 = PREFIXOIDS.extract_frequency(PREFIXOIDS.split_word_at_pipe(i[1])[1], feature_3_prefixoids_heads)
+        item_5 = PREFIXOIDS.calculate_cosine_similarity(i[0], PREFIXOIDS.split_word_at_pipe(i[1])[0], feature_5_prefixoid_vector_dict)
         feature_1_prefixoids.append(item_1)
         feature_2_prefixoids.append(item_2)
         feature_3_prefixoids.append(item_3)
+        feature_4_prefixoids.append(item_4)
+        feature_5_prefixoids.append(item_5)
+    # print(feature_1_prefixoids)
     # print(len(feature_1_prefixoids))
+    # print(feature_2_prefixoids)
     # print(len(feature_2_prefixoids))
+    # print(feature_3_prefixoids)
     # print(len(feature_3_prefixoids))
+    # print(feature_4_prefixoids)
+    # print(len(feature_4_prefixoids))
+    # print(feature_5_prefixoids)
+    # print(len(feature_5_prefixoids))
 
     # print(feature_1_prefixoids[50], feature_2_prefixoids[50], feature_3_prefixoids[50])
+    # PREFIXOIDS.write_list_to_file(prefixoid_inventory, 'prefix-out.txt')
 
     """
         SUFFIXOIDS
@@ -361,19 +461,36 @@ if __name__ == "__main__":
     feature_1_suffixoids = []  # binary indicator, if affixoid
     feature_2_suffixoids = []  # frequency
     feature_3_suffixoids = []
+    feature_4_suffixoids = []
+    feature_5_suffixoids = []
+    feature_3_suffixoids_formations = PREFIXOIDS.create_frequency_dictionary(DATA_PATH + FREQUENCY_SUFFIXOID_FORMATIONS)
     feature_3_suffixoids_lemmas = SUFFIXOIDS.create_frequency_dictionary(DATA_PATH + FREQUENCY_SUFFIXOID_LEMMAS)
     feature_3_suffixoids_modifiers = SUFFIXOIDS.create_frequency_dictionary(DATA_PATH + FREQUENCY_SUFFIXOID_MODIFIERS)
+    feature_5_suffixoids_vector_dict = SUFFIXOIDS.create_vector_dictionary(DATA_RESSOURCES_PATH + FAST_TEXT_SUFFIXOID_VECTORS)
 
     for i in suffixoid_inventory:
         item_1 = SUFFIXOIDS.transform_to_binary(i[-1])
-        item_2 = SUFFIXOIDS.extract_frequency(i[-3], feature_3_suffixoids_lemmas)
-        item_3 = SUFFIXOIDS.extract_frequency(SUFFIXOIDS.split_word_at_pipe(i[1])[0], feature_3_suffixoids_modifiers)
+        item_2 = SUFFIXOIDS.extract_frequency(i[0], feature_3_suffixoids_formations)
+        item_3 = SUFFIXOIDS.extract_frequency(i[-3], feature_3_suffixoids_lemmas)
+        item_4 = SUFFIXOIDS.extract_frequency(SUFFIXOIDS.split_word_at_pipe(i[1])[0], feature_3_suffixoids_modifiers)
+        item_5 = SUFFIXOIDS.calculate_cosine_similarity(i[0], SUFFIXOIDS.split_word_at_pipe(i[1])[1], feature_5_suffixoids_vector_dict)
         feature_1_suffixoids.append(item_1)
         feature_2_suffixoids.append(item_2)
         feature_3_suffixoids.append(item_3)
+        feature_4_suffixoids.append(item_4)
+        feature_5_suffixoids.append(item_5)
+    # print(feature_1_suffixoids)
     # print(len(feature_1_suffixoids))
+    # print(feature_2_suffixoids)
     # print(len(feature_2_suffixoids))
+    # print(feature_3_suffixoids)
     # print(len(feature_3_suffixoids))
+    # print(feature_4_suffixoids)
+    # print(len(feature_4_suffixoids))
+    print(feature_5_suffixoids)
+    print(len(feature_5_suffixoids))
+
+    # SUFFIXOIDS.write_list_to_file(suffixoid_inventory, 'suffix-out.txt')
 
     # counter = 0
     # features = []
@@ -386,4 +503,3 @@ if __name__ == "__main__":
     #     counter += 1
     # print(features)
     # print(feature_1_suffixoids[233], feature_2_suffixoids[233], feature_3_suffixoids[233])
-
