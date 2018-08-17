cfed6ae619f6c8c025adf3cd8d0b50940bd52494
Darmin Spahic <Spahic@cl.uni-heidelberg.de>
2018-08-17 10:34:57 +0200

    Refactoring

--- src/Classifier.py
+++ src/Classifier.py
@@ -46,6 +46,87 @@ config._interpolation = configparser.ExtendedInterpolation()
 config.read('config.ini')
 
 
+def grid_search_report(X_train, y_train, X_test, y_test, affixoid_class):
+
+    import warnings
+    warnings.filterwarnings("ignore")
+
+    tuned_parameters = [{'kernel': ['rbf'], 'gamma': [1e-5, 1e-4, 1e-3, 1e-2, 0.1], 'C': [1, 10, 100, 1000]},
+                        {'kernel': ['linear'], 'C': [1, 10, 100, 1000]}]
+
+    scores = ['precision', 'recall']
+
+    for score in scores:
+        print("Tuning hyper-parameters for %s" % score)
+        print()
+
+        clf = GridSearchCV(svm.SVC(), tuned_parameters, cv=5, scoring='%s_macro' % score)
+        clf.fit(X_train, y_train)
+
+        print("Best parameters set found on development set for:", affixoid_class)
+        print()
+        print(clf.best_params_)
+        print()
+        print("Grid scores on development set:")
+        print()
+        means = clf.cv_results_['mean_test_score']
+        stds = clf.cv_results_['std_test_score']
+        for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+            print("%0.3f (+/-%0.03f) for %r" % (mean, std * 2, params))
+        print()
+
+        print("Detailed classification report:")
+        print()
+        print("The model is trained on the full development set.")
+        print("The scores are computed on the full evaluation set.")
+        print()
+        y_true, y_pred = y_test, clf.predict(X_test)
+        print(classification_report(y_true, y_pred))
+        print()
+
+
+def cross_validate(clf, instances, labels):
+    cv = ShuffleSplit(n_splits=5, test_size=0.3, random_state=5)
+    scores = cross_val_score(clf, instances, labels, cv=cv)
+    print('5-Fold crossvalidation:', scores)
+
+
+def print_scores(score_title, classifier, classifer_results, test_instances, test_labels, baseline_classifier):
+    # print()
+    # print(Style.BOLD + 'RESULTS' + Style.END)
+    # print(classifer_results)
+    print()
+    print(Style.BOLD + 'SCORES', score_title + Style.END)
+    print('Classifier score: ', classifier.score(test_instances, test_labels))
+    # print('Precision: ', precision_score(test_labels, classifer_results))
+    # print('Recall: ', recall_score(test_labels, classifer_results))
+    # print('F-1 Score: ', f1_score(test_labels, classifer_results, average='weighted'))
+    print()
+    target_names = ['affixoid', 'non-affixoid']
+    print(classification_report(test_labels, classifer_results, target_names=target_names))
+    print('\nConfusion matrix:')
+    print(confusion_matrix(test_labels, classifer_results))
+    print('\nMost frequent sense baseline')
+    print(baseline_classifier.score(test_instances, test_labels))
+    print()
+
+
+def plot(y_test, y_score):
+    average_precision = average_precision_score(y_test, y_score)
+    precision, recall, _ = precision_recall_curve(y_test, y_score)
+
+    plt.step(recall, precision, color='b', alpha=0.2, where='post')
+    plt.fill_between(recall, precision, step='post', alpha=0.2, color='b')
+
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.ylim([0.0, 1.05])
+    plt.xlim([0.0, 1.0])
+    plt.title('2-class Precision-Recall curve: AP={0:0.2f}'.format(average_precision))
+
+    plt.show()
+
+
 class Style:
     BOLD = '\033[1m'
     END = '\033[0m'
@@ -121,45 +202,8 @@ if __name__ == "__main__":
     X_train_suff, X_test_suff, y_train_suff, y_test_suff = train_test_split(suff_X_scaled, suff_y, test_size=0.3, random_state=5, shuffle=True)
 
     """ Parameter Selection with GridSearch """
-    def grid_search_report(X_train, y_train, X_test, y_test, affixoid_class):
-        import warnings
-        warnings.filterwarnings("ignore")
-
-        tuned_parameters = [{'kernel': ['rbf'], 'gamma': [1e-5, 1e-4, 1e-3, 1e-2, 0.1], 'C': [1, 10, 100, 1000]},
-                            {'kernel': ['linear'], 'C': [1, 10, 100, 1000]}]
-
-        scores = ['precision', 'recall']
-
-        for score in scores:
-            print("Tuning hyper-parameters for %s" % score)
-            print()
-
-            clf = GridSearchCV(svm.SVC(), tuned_parameters, cv=5, scoring='%s_macro' % score)
-            clf.fit(X_train, y_train)
-
-            print("Best parameters set found on development set for:", affixoid_class)
-            print()
-            print(clf.best_params_)
-            print()
-            print("Grid scores on development set:")
-            print()
-            means = clf.cv_results_['mean_test_score']
-            stds = clf.cv_results_['std_test_score']
-            for mean, std, params in zip(means, stds, clf.cv_results_['params']):
-                print("%0.3f (+/-%0.03f) for %r" % (mean, std * 2, params))
-            print()
-
-            print("Detailed classification report:")
-            print()
-            print("The model is trained on the full development set.")
-            print("The scores are computed on the full evaluation set.")
-            print()
-            y_true, y_pred = y_test, clf.predict(X_test)
-            print(classification_report(y_true, y_pred))
-            print()
-
-    # grid_search_report(X_train_pref, y_train_pref, X_test_pref, y_test_pref, 'Prefixoids')
-    # grid_search_report(X_train_suff, y_train_suff, X_test_suff, y_test_suff, 'Suffixoids')
+    grid_search_report(X_train_pref, y_train_pref, X_test_pref, y_test_pref, 'Prefixoids')
+    grid_search_report(X_train_suff, y_train_suff, X_test_suff, y_test_suff, 'Suffixoids')
 
     """ SVM """
     clf_pref = svm.SVC(kernel="rbf", gamma=0.01, C=100).fit(X_train_pref, y_train_pref)  # gamma=0.01, C=100
@@ -172,51 +216,12 @@ if __name__ == "__main__":
     results_suff = clf_suff.predict(X_test_suff)
 
     """ Crossvalidation """
-    def cross_validate(clf, instances, labels):
-        cv = ShuffleSplit(n_splits=5, test_size=0.3, random_state=5)
-        scores = cross_val_score(clf, instances, labels, cv=cv)
-        print('5-Fold crossvalidation:', scores)
-
     """ SCORES """
-    def print_scores(score_title, classifier, classifer_results, test_instances, test_labels, baseline_classifier):
-        # print()
-        # print(Style.BOLD + 'RESULTS' + Style.END)
-        # print(classifer_results)
-        print()
-        print(Style.BOLD + 'SCORES', score_title + Style.END)
-        print('Classifier score: ', classifier.score(test_instances, test_labels))
-        # print('Precision: ', precision_score(test_labels, classifer_results))
-        # print('Recall: ', recall_score(test_labels, classifer_results))
-        # print('F-1 Score: ', f1_score(test_labels, classifer_results, average='weighted'))
-        print()
-        target_names = ['affixoid', 'non-affixoid']
-        print(classification_report(test_labels, classifer_results, target_names=target_names))
-        print('\nConfusion matrix:')
-        print(confusion_matrix(test_labels, classifer_results))
-        print('\nMost frequent sense baseline')
-        print(baseline_classifier.score(test_instances, test_labels))
-        print()
-
     print_scores('Prefixoids', clf_pref, results_pref, X_test_pref, y_test_pref, clf_dummy_pref)
     cross_validate(svm.SVC(kernel="rbf", gamma=0.01, C=100), pref_X_scaled, pref_y)
 
     print_scores('Suffixoids', clf_suff, results_suff, X_test_suff, y_test_suff, clf_dummy_suff)
     cross_validate(svm.SVC(kernel="rbf", gamma=0.1, C=10), suff_X_scaled, suff_y)
-
-    def plot(y_test, y_score):
-        average_precision = average_precision_score(y_test, y_score)
-        precision, recall, _ = precision_recall_curve(y_test, y_score)
-
-        plt.step(recall, precision, color='b', alpha=0.2, where='post')
-        plt.fill_between(recall, precision, step='post', alpha=0.2, color='b')
-
-        plt.xlabel('Recall')
-        plt.ylabel('Precision')
-        plt.ylim([0.0, 1.05])
-        plt.xlim([0.0, 1.0])
-        plt.title('2-class Precision-Recall curve: AP={0:0.2f}'.format(average_precision))
-
-        plt.show()
 
     # plot(y_test_pref, results_pref)
     # plot(y_test_suff, results_suff)
